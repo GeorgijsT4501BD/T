4edd323898376c9880c68fb2c866fa2f955e5859
GeorgijsT4501BD <32352474+GeorgijsT4501BD@users.noreply.github.com>
2017-12-11 00:49:00 +0200

    Add files via upload

---
+++ oop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecrypt_Serialize_BD
+{
+    interface MovementSpeed
+    {
+        void Speed();
+    }
+    public abstract class Vehicle
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public abstract void Summ();// may be not realized but have to be in subclasses
+        public virtual void Abc()//must be realized but may not to be in sub classes
+        {
+        }
+        public double NotSumm()
+        {
+            return a / b;
+        }
+    }
+
+    public class Car : Vehicle, MovementSpeed
+    {
+        //this class gets Vehicles fields and methods
+        public void Speed()
+        {
+        }
+        public override void Summ()
+        {
+            Console.Write("qwerty", base.NotSumm());
+        }
+    }
+}
